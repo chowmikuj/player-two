592099fa85d89b7c062217cf1b7d6955f4aadb13
hushpuppy <140564473+hushpuppiesss@users.noreply.github.com>
2025-10-15 06:44:59 -0400

    ack

--- player-two/scenes/changer.gd
+++ player-two/scenes/changer.gd
@@ -6,6 +6,5 @@ func _on_arcade_pressed() -> void:
 func _on_lan_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/2.tscn")
 
-
 func _on_online_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/3.tscn")
